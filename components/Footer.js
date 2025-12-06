fec916b0777144aa4bc4e582c8b209a8026e978e
vikasmv <itsviksmv@gmail.com>
2025-12-06 22:58:02 +0530

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-blue-600 text-white py-6 text-center">
+      &copy; 2025 Flowlient. All rights reserved.
+    </footer>
+  );
+}
